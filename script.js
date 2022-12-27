1af452ed25671f84491b185859c0a3e7066ee98f
Jay Gajera <jaygajera2001@gmail.com>
2022-12-27 17:30:16 +0530

    16

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // playlist = {
 //   'song_1': 'song/005.mp3',
-  
+
 // }
 
 var play = document.getElementById("play");
-var music =  document.querySelector("audio");
+var music = document.querySelector("audio");
 const img = document.getElementById("123");
 var extra = document.getElementById("extra");
 var playPaushBtn = document.getElementById("playPaushBtn");
@@ -47,52 +47,70 @@ extraSecond.classList.add("pulse")
 //  }
 //  );
 
+play.classList.replace('bi-pause-circle', 'bi-play-circle');
+playSecond.classList.replace('bi-pause-circle', 'bi-play-circle');
 
 
 
 //for pause to play btn
-let isplaying = true;
- const pausemusic = () =>{
+let isplaying = false;
+const pausemusic = () => {
   isplaying = false;
   music.pause();
-  play.classList.replace( 'bi-pause-circle','bi-play-circle');
-  playSecond.classList.replace( 'bi-pause-circle','bi-play-circle');
+  play.classList.replace('bi-pause-circle', 'bi-play-circle');
+  playSecond.classList.replace('bi-pause-circle', 'bi-play-circle');
   img.classList.remove("anime");
   extra.classList.remove("pulse");
   extraSecond.classList.remove("pulse");
- };
+};
 
 //for play to payse btn
-const playmusic = () =>{
+const playmusic = () => {
   isplaying = true;
   music.play();
-  play.classList.replace( 'bi-play-circle','bi-pause-circle');
-  playSecond.classList.replace( 'bi-play-circle','bi-pause-circle');
+  play.classList.replace('bi-play-circle', 'bi-pause-circle');
+  playSecond.classList.replace('bi-play-circle', 'bi-pause-circle');
   img.classList.add("anime");
   extra.classList.add("pulse");
   extraSecond.classList.add("pulse");
- };
+};
 
- playPaushBtn.addEventListener('click', () => {
-  if(isplaying == true){
+playPaushBtn.addEventListener('click', () => {
+  if (isplaying == true) {
     pausemusic();
   }
-  else{
+  else {
     playmusic();
   }
- }
- );
+}
+);
 
- playPaushBtnSecond.addEventListener('click', () => {
-  if(isplaying == true){
+playPaushBtnSecond.addEventListener('click', () => {
+  if (isplaying == true) {
     pausemusic();
   }
-  else{
+  else {
     playmusic();
   }
- }
- );
+}
+);
 
+function Check() {
+  music.onplaying = function () {
+    play.classList.replace('bi-play-circle', 'bi-pause-circle');
+    playSecond.classList.replace('bi-play-circle', 'bi-pause-circle');
+    isplaying = true
+  }
+  music.onpause = function () {
+    isplaying = false;
+    play.classList.replace('bi-pause-circle', 'bi-play-circle');
+    playSecond.classList.replace('bi-pause-circle', 'bi-play-circle');
+
+  }
+}
+
+
+Check();
 
 window.onpause = function () {
   pausemusic();
@@ -133,18 +151,18 @@ $(document).ready(function () {
 
 // PROGRESSBAR
 
-let processScroll=()=>{
-  let docElem=document.documentElement,
-  docBody=document.body,
-  scrollTop=docElem["scrollTop"] || docBody["scrollTop"],
-  scrollBottom=(docElem["scrollHeight"] || docBody["scrollHeight"])-window.innerHeight,
-  scrollPercent=scrollTop/scrollBottom * 100 + "%";
+let processScroll = () => {
+  let docElem = document.documentElement,
+    docBody = document.body,
+    scrollTop = docElem["scrollTop"] || docBody["scrollTop"],
+    scrollBottom = (docElem["scrollHeight"] || docBody["scrollHeight"]) - window.innerHeight,
+    scrollPercent = scrollTop / scrollBottom * 100 + "%";
 
-  document.getElementById("progress-bar").style.setProperty("--scrollAmount",scrollPercent);
+  document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent);
 }
 
 
-document.addEventListener("scroll",processScroll);
+document.addEventListener("scroll", processScroll);
 
 
 
@@ -172,12 +190,12 @@ document.addEventListener("scroll",processScroll);
 // });
 
 const countEl = document.getElementById("count");
-      countvisits();
+countvisits();
 
-      function countvisits() {
-        fetch('https://api.countapi.xyz/update/shreenathjigrafix.in/Invitation/?amount=1')
-          .then((res) => res.json())
-          .then((res) => {
-            countEl.innerHTML = res.value;
-          });
-      }
+function countvisits() {
+  fetch('https://api.countapi.xyz/update/shreenathjigrafix.in/Invitation/?amount=1')
+    .then((res) => res.json())
+    .then((res) => {
+      countEl.innerHTML = res.value;
+    });
+}
